626c64085318ef237cb7312c613e55a6c409b6ec
lchrisdion <chrisdionleon80@gmail.com>
2022-06-10 20:47:19 +0700

    :sparkles: first commit

---
+++ lib/app/ui/theme/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appThemeData = ThemeData(
+  useMaterial3: true,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      padding: const EdgeInsets.symmetric(
+        vertical: 15,
+      ),
+    ),
+  ),
+);
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:simple_weather_app/app/modules/home/bindings/home_binding.dart';
+import 'package:simple_weather_app/app/ui/theme/app_theme.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialBinding: HomeBinding(),
+      initialRoute: Routes.HOME,
+      navigatorKey: Get.key,
+      theme: appThemeData,
+    );
+  }
+}
